515583a00dc4d518e76406798f9536d184cecebc
sameersheikh2 <sameersheih819@gmail.com>
2021-09-28 17:52:53 +0530

    conection success to database

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php
+if(isset($_POST['name'])){
+$server = "localhost";
+$name = "root";
+$password = "";
+$con = mysqli_connect($server, $name, $password);
+
+if(!$con){
+    die("connection to this database is failed". mysqli_connect_error());
+}
+// echo "Success";
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$sql = "INSERT INTO `sonyinfo`.`sonyinfo`(`Name`, `Email`, `Message`, `Date`) VALUES ('$name', '$email', '$message', current_timestamp());"; 
+
+// echo $sql;
+
+if($con->query($sql) == true){
+    // echo "Successfully inserted";
+}
+else{
+    echo "ERROR: $sql <br> $con->error";
+}
+
+$con->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
